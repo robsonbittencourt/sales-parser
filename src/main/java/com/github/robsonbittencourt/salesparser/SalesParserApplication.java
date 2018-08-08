28c97bb471a9ea09390c85f5426d33fce3a7ff15
robsonbittencourt <robson.luizv@gmail.com>
2018-08-07 22:17:46 -0300

    Add initial project structure

---
+++ src/main/java/com/github/robsonbittencourt/salesparser/SalesParserApplication.java
@@ -0,0 +1,12 @@
+package com.github.robsonbittencourt.salesparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesParserApplication.class, args);
+	}
+}
